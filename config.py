88f87e9b79bb612b3fa5fda0e5f956472025cf20
LimXD2003 <limyuhang022@gmail.com>
2024-12-26 13:18:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c1m2cy2wolix.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limyuhang-employee"
+customregion = "us-east-1"
+
